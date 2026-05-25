71dd78719cab92712a3132f5d6eac1a4b496015f
SamuelCastroUPTC <castro.samuel@uptc.edu.co>
2026-05-24 19:12:45 -0500

    fix:MASTERCARD delivery details

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = require('./src/app');
 const logger = require('./src/utils/logger');
 
 // Puerto en el que escucha el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   logger.info(`Servidor de Mastercard Payment iniciado en puerto ${PORT}`);
